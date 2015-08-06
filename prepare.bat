4199d9c41da0720fc7bafd675f49858c43246a65
ewancoder <ewancoder@gmail.com>
2015-08-06 12:25:54 +0300

    Add Release folder for Russian version as well.

--- prepare.bat
+++ prepare.bat
@@ -5,4 +5,6 @@ cd _build\latex
 "c:\Program Files (x86)\MiKTeX 2.9\miktex\bin\pdflatex.exe" ModemManager.tex
 "c:\Program Files (x86)\MiKTeX 2.9\miktex\bin\pdflatex.exe" ModemManager.tex
 cd ..\..
+
+mkdir Release
 copy /Y _build\latex\ModemManager.pdf Release\ModemManager-ru.pdf
