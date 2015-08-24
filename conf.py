719cc0bccafded30101538edef2e6281462d3176
ewancoder <ewancoder@gmail.com>
2015-08-24 10:09:43 +0300

    Fixed typo. Tehnikon -> Technikon.

--- conf.py
+++ conf.py
@@ -205,7 +205,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'ModemManager.tex', 'Modem Manager Documentation',
-   'Technikon', 'report'),
+   'Technikon', 'report'), # 'Technikon, Ewancoder'
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
